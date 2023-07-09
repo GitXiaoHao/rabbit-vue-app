5e1f8c57b11803af69b33000e3e34524b21b4371
yu <2486245007@qq.com>
2023-07-09 18:07:50 +0800

    home的商品推荐组件封装

--- src/views/home/components/HomeBanner.vue
+++ src/views/home/components/HomeBanner.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <img src="https://dogefs.s3.ladydaily.com/~/source/wallhaven/full/we/wallhaven-weeqzx.jpg?w=2560&h=1440&fmt=webp" alt="item"/>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +13,16 @@
 </script>
 
 <style scoped lang="scss">
-
+  .home-banner{
+    width: 1240px;
+    height: 500px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 98;
+    img{
+      width: 100%;
+      height: 500px;
+    }
+  }
 </style>
--- src/views/home/components/HomeNew.vue
+++ src/views/home/components/HomeNew.vue
@@ -1,11 +1,57 @@
 <template>
-
+  <HomePanel title="新鲜好物" sub-title="新鲜出路 品质靠谱">
+    <ul class="goods-list">
+      <li v-for="item in 4" :key="item">
+        <RouterLink to="/">
+          <img src="" alt="">
+          <p class="name">name</p>
+          <p class="price">&yen;200.00</p>
+        </RouterLink>
+      </li>
+    </ul>
+  </HomePanel>
 </template>
 
 <script setup>
 
+import HomePanel from "@/views/home/components/HomePanel.vue";
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="less">
+// 鼠标经过上移阴影动画
+.hoverShadow () {
+  transition: all 0.5s;
+  &:hover {
+    transform: translate3d(0, -3px, 0);
+    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2);
+  }
+}
 
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+    background: #f0f9f4;
+    .hoverShadow();
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding: 12px 30px 0 30px;
+      text-align: center;
+    }
+
+    .price {
+      color: #cf4444;
+    }
+  }
+}
 </style>
